c560eee51ea27b953d32e53a2930b27c01ef05f9
lopezchin <lopezchin@yahoo.com>
2023-11-10 03:18:58 +0800

    update master

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,34 +1,54 @@
 document.getElementById('saveContactBtn').addEventListener('click', function(e) {
   e.preventDefault();
+  
+  var name = 'Archie Lopez';
+  var phoneNumber = '+639664007858';
+  var email = 'lopezchin08@gmail.com';
 
-  generateVCard();
+  // Prepare contact information
+  var contactInfo = `Name: ${name}\nPhone: ${phoneNumber}\nEmail: ${email}`;
 
+  // Copy to clipboard
+  navigator.clipboard.writeText(contactInfo)
+    .then(function() {
+      alert('Contact information copied to clipboard.');
+    })
+    .catch(function(err) {
+      console.error('Error copying to clipboard: ', err);
+    });
 });
 
+// document.getElementById('saveContactBtn').addEventListener('click', function(e) {
+//   e.preventDefault();
 
-function generateVCard() {
-    // Get contact information from the user
-    var fullname = "Archie Lopez";
-    var phoneNumber = "+639664007858";
-    var email = "lopezchin08@gmail.com";
+//   generateVCard();
 
-    // Create vCard content
-    var vCardData = `BEGIN:VCARD
-                      VERSION:3.0
-                      FN:`+fullname+`
-                      TEL:`+phoneNumber+`
-                      EMAIL:`+email+`
-                      END:VCARD`;
+// });
 
-    // Create a Blob containing the vCard data
-    var blob = new Blob([vCardData], { type: 'text/vcard' });
 
-    // Create a download link
-    var link = document.createElement('a');
-    link.href = window.URL.createObjectURL(blob);
-    link.download = fullname+'.vcf';
-    link.click();
-}
+// function generateVCard() {
+//     // Get contact information from the user
+//     var fullname = "Archie Lopez";
+//     var phoneNumber = "+639664007858";
+//     var email = "lopezchin08@gmail.com";
+
+//     // Create vCard content
+//     var vCardData = `BEGIN:VCARD
+//                       VERSION:3.0
+//                       FN:`+fullname+`
+//                       TEL:`+phoneNumber+`
+//                       EMAIL:`+email+`
+//                       END:VCARD`;
+
+//     // Create a Blob containing the vCard data
+//     var blob = new Blob([vCardData], { type: 'text/vcard' });
+
+//     // Create a download link
+//     var link = document.createElement('a');
+//     link.href = window.URL.createObjectURL(blob);
+//     link.download = fullname+'.vcf';
+//     link.click();
+// }
 
 // Call the function when the page is loaded (for testing purposes)
 // generateVCard();

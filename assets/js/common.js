09b1cd78d00dc894890030a239eb4062c0551ef3
lopezchin <lopezchin@yahoo.com>
2023-11-10 03:39:21 +0800

    update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,54 +1,66 @@
-document.getElementById('saveContactBtn').addEventListener('click', function(e) {
-  e.preventDefault();
-  
-  var name = 'Archie Lopez';
-  var phoneNumber = '+639664007858';
-  var email = 'lopezchin08@gmail.com';
-
-  // Prepare contact information
-  var contactInfo = `Name: ${name}\nPhone: ${phoneNumber}\nEmail: ${email}`;
-
-  // Copy to clipboard
-  navigator.clipboard.writeText(contactInfo)
-    .then(function() {
-      alert('Contact information copied to clipboard.');
-    })
-    .catch(function(err) {
-      console.error('Error copying to clipboard: ', err);
-    });
-});
-
 // document.getElementById('saveContactBtn').addEventListener('click', function(e) {
 //   e.preventDefault();
 
-//   generateVCard();
+//   var fullName = 'Archie Lopez';
+//   var phoneNumber = '+639664007858';
+//   var email = 'lopezchin08@gmail.com';
+
+//   saveToPhonebook(fullName, phoneNumber);
 
 // });
 
+// function saveToPhonebook(fullName, phoneNumber) {
+//     // Create a new Contact object
 
-// function generateVCard() {
-//     // Get contact information from the user
-//     var fullname = "Archie Lopez";
-//     var phoneNumber = "+639664007858";
-//     var email = "lopezchin08@gmail.com";
+//     var contact = navigator.contacts.create();
 
-//     // Create vCard content
-//     var vCardData = `BEGIN:VCARD
-//                       VERSION:3.0
-//                       FN:`+fullname+`
-//                       TEL:`+phoneNumber+`
-//                       EMAIL:`+email+`
-//                       END:VCARD`;
+//     // Set the contact properties
+//     contact.displayName = fullName;
+//     contact.nickname = fullName;
 
-//     // Create a Blob containing the vCard data
-//     var blob = new Blob([vCardData], { type: 'text/vcard' });
+//     var phoneNumbers = [];
+//     phoneNumbers[0] = new ContactField('mobile', phoneNumber, true); // true means preferred number
+//     contact.phoneNumbers = phoneNumbers;
 
-//     // Create a download link
-//     var link = document.createElement('a');
-//     link.href = window.URL.createObjectURL(blob);
-//     link.download = fullname+'.vcf';
-//     link.click();
+//     // Save the contact to the device
+//     contact.save(function(contact) {
+//         console.log("Contact saved:", contact);
+//     }, function(error) {
+//         console.error("Error saving contact:", error);
+//     });
 // }
+
+document.getElementById('saveContactBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  generateVCard();
+
+});
+
+
+function generateVCard() {
+    // Get contact information from the user
+    var fullname = "Archie Lopez";
+    var phoneNumber = "09664007858";
+    var email = "lopezchin08@gmail.com";
+
+    // Create vCard content
+    var vCardData = `BEGIN:VCARD
+                      VERSION:3.0
+                      FN:`+fullname+`
+                      TEL:`+phoneNumber+`
+                      EMAIL:`+email+`
+                      END:VCARD`;
+
+    // Create a Blob containing the vCard data
+    var blob = new Blob([vCardData], { type: 'text/vcard' });
+
+    // Create a download link
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fullname+'.vcf';
+    link.click();
+}
 
 // Call the function when the page is loaded (for testing purposes)
 // generateVCard();

669b7bd265d3e726628c4cb1132db737fe45a842
lopezchin <lopezchin@yahoo.com>
2023-11-10 03:03:27 +0800

    update download vcf file

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,36 +1,34 @@
-document.getElementById('saveContactBtn').addEventListener('click', function() {
-  // Replace the following URL with the actual endpoint to handle the contact saving on the server
-  var saveContactEndpoint = 'https://example.com/saveContact';
+document.getElementById('saveContactBtn').addEventListener('click', function(e) {
+  e.preventDefault();
 
-  // Replace the following data with the actual contact details you want to save
-  var contactData = {
-    name: 'Archie Lopez',
-    phone: '+639664007858',
-    email: 'lopezchin08@gmail.com'
-  };
+  generateVCard();
 
-  // Use fetch to send a POST request to the server
-  fetch(saveContactEndpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(contactData),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Handle the response from the server (if needed)
-    console.log('Contact saved successfully:', data);
-    alert('Contact saved successfully!');
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('Error saving contact:', error);
-    alert('Error saving contact. Please try again.');
-  });
 });
+
+
+function generateVCard() {
+    // Get contact information from the user
+    var fullname = "Archie Lopez";
+    var phoneNumber = "+639664007858";
+    var email = "lopezchin08@gmail.com";
+
+    // Create vCard content
+    var vCardData = `BEGIN:VCARD
+                      VERSION:3.0
+                      FN:`+fullname+`
+                      TEL:`+phoneNumber+`
+                      EMAIL:`+email+`
+                      END:VCARD`;
+
+    // Create a Blob containing the vCard data
+    var blob = new Blob([vCardData], { type: 'text/vcard' });
+
+    // Create a download link
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fullname+'.vcf';
+    link.click();
+}
+
+// Call the function when the page is loaded (for testing purposes)
+// generateVCard();
